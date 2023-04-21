9d82f4f2fdb03eacb914f27a8f4b5991e03164a9
Raghu Thotakara S <raghu.thotakara.dms@crowdbotics.com>
2023-04-21 18:54:44 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/product-listing/index.js
+++ screens/product-listing/index.js
@@ -1,9 +1,13 @@
-import React from "react";
+import { useNavigation } from "@react-navigation/native";
+import { Pressable } from "react-native";
 import { Text, StyleSheet, View } from "react-native";
 
-const ProductListingScreen = params => {
+const ProductListingScreen = () => {
+  const navigation = useNavigation();
   return <View style={styles.container}>
-      <Text style={styles.headingTxt}>Product Listing Screen</Text>
+      <Pressable onPress={() => {
+      navigation.navigate("eventHome1");
+    }}><Text style={styles.headingTxt}>Product Details</Text></Pressable>
     </View>;
 };
 
@@ -17,7 +21,10 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: "bold",
     padding: 2,
-    marginVertical: 12
+    marginVertical: 12,
+    lineHeight: 27,
+    textAlign: "center",
+    fontFamily: "Roboto"
   }
 });
 export default ProductListingScreen;
